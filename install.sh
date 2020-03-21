d1a39aa213d3e2f2da8056ef62c31b319967594c
Norhan Abozeed <48640788+norhantharwat@users.noreply.github.com>
2020-03-21 20:26:25 +0200

    Fix chrome-sandbox Issue
    the application may not start due chrome-sandbox ownership and permission issue

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ echo $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 tar xvf $PD/Lotion-linux-x64.tar.xz -C $INSTALL_DIR --strip 1
 /bin/bash $PD/create_shortcut.sh
+##you my need to add the following lines to make it work
+chown root:root $PD/Lotion/chrome-sandbox
+chmod 4755 $PD/Lotion/chrome-sandbox
