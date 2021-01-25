67d3e6fea63f6ae64611bd05ca8d8fd5c85409b6
joshu <joshuapetitma@yahoo.com>
2021-01-25 10:07:26 -0500

    Removes no longer present install_native.sh file.

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 # This scripts removes files that are not needed for lotion runtime
 
-rm ./create_shortcut.sh ./install.sh ./install_native.sh ./Lotion-linux-x64.tar.xz
+rm ./create_shortcut.sh ./install.sh ./Lotion-linux-x64.tar.xz
 rm -rf ./nativeApp
